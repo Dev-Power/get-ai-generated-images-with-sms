f8923485e4aec09c9ea937b43bb1987ffd63befa
dev <dev@example.com>
2022-11-17 10:27:34 +0000

    final implementation

--- src/Dalle2ImageSmsApi/Controllers/IncomingSmsController.cs
+++ src/Dalle2ImageSmsApi/Controllers/IncomingSmsController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using OpenAI.GPT3.Interfaces;
+using OpenAI.GPT3.ObjectModels.RequestModels;
 using Twilio.AspNet.Core;
 using Twilio.TwiML;
 using Twilio.TwiML.Messaging;
@@ -9,18 +11,36 @@ namespace Dalle2ImageSmsApi.Controllers;
 [Route("[controller]")]
 public class IncomingSmsController : TwilioController
 {
+    private readonly IOpenAIService _openAiService;
+
+    public IncomingSmsController(IOpenAIService openAiService)
+    {
+        _openAiService = openAiService;
+    }
+    
     [HttpPost]
     public async Task<TwiMLResult> Index()
     {
         var form = await Request.ReadFormAsync();
         var incomingText = form["Body"];
-        
+
+        var createImageRequest = new ImageCreateRequest
+        {
+            Size = "1024x1024",
+            N = 1,
+            Prompt = incomingText,
+            ResponseFormat = "url"
+        };
+
+        var createImageResponse = await _openAiService.Image.CreateImage(createImageRequest);
+        var image = createImageResponse.Results.First();
+
         var message = new Message();
         message.Body($"Here's the image for your query: {incomingText}");
-        message.Media(new Uri("https://picsum.photos/1024/1024"));
+        message.Media(new Uri(image.Url));
 
         var messagingResponse = new MessagingResponse();
-        messagingResponse.Message(message);
+        messagingResponse.Append(message);
         
         return TwiML(messagingResponse);
     }
