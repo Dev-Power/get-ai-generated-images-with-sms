0f723e4ac4595ef74361bfadf9db00c00da197e7
dev <dev@example.com>
2023-01-10 05:49:52 +0000

    refactor: update the controller

--- src/Dalle2ImageSmsApi/Controllers/IncomingSmsController.cs
+++ src/Dalle2ImageSmsApi/Controllers/IncomingSmsController.cs
@@ -4,17 +4,22 @@ using OpenAI.GPT3.ObjectModels.RequestModels;
 using Twilio.AspNet.Core;
 using Twilio.TwiML;
 using Twilio.TwiML.Messaging;
-
+ 
 namespace Dalle2ImageSmsApi.Controllers;
-
+ 
 [ApiController]
 [Route("[controller]")]
 public class IncomingSmsController : TwilioController
 {
+    private readonly ILogger<IncomingSmsController> _logger;
     private readonly IOpenAIService _openAiService;
-
-    public IncomingSmsController(IOpenAIService openAiService)
+ 
+    public IncomingSmsController(
+        ILogger<IncomingSmsController> logger, 
+        IOpenAIService openAiService
+    )
     {
+        _logger = logger;
         _openAiService = openAiService;
     }
     
@@ -23,7 +28,7 @@ public class IncomingSmsController : TwilioController
     {
         var form = await Request.ReadFormAsync();
         var incomingText = form["Body"];
-
+ 
         var createImageRequest = new ImageCreateRequest
         {
             Size = "1024x1024",
@@ -31,17 +36,27 @@ public class IncomingSmsController : TwilioController
             Prompt = incomingText,
             ResponseFormat = "url"
         };
-
+ 
         var createImageResponse = await _openAiService.Image.CreateImage(createImageRequest);
+        if(!createImageResponse.Successful)
+        {
+            var errorMessage = "An error occurred trying to create OpenAI image." +
+                               $" {createImageResponse.Error.Code}: {createImageResponse.Error.Message}.";
+            _logger.LogError(errorMessage);
+            
+            return new MessagingResponse()
+                .Message("An unexpected error occurred. Try again later.")
+                .ToTwiMLResult();
+        }
+ 
         var image = createImageResponse.Results.First();
-
+ 
         var message = new Message();
         message.Body($"Here's the image for your query: {incomingText}");
         message.Media(new Uri(image.Url));
-
-        var messagingResponse = new MessagingResponse();
-        messagingResponse.Append(message);
-        
-        return TwiML(messagingResponse);
+ 
+        return new MessagingResponse()
+            .Append(message)
+            .ToTwiMLResult();
     }
 }
